130ce50dc94d15bccd9a50ac900cac2d3bfeab45
Alejandro1220 <70906172+Alejandro1220@users.noreply.github.com>
2022-04-19 08:20:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="estilos.css">
+	<title>Learn english</title>
+</head>
+<body>
+
+     <div class="titulo">Learn english</div>
+     <div class="hipers">
+     	<a href="">Login</a>
+     	 <a href="">Register</a>
+     	</div>
+
+     
+     <div class="carrusel">carrusel</div>
+     <div>
+     	
+     </div>
+
+</body>
+</html>
